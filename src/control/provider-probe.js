72f8627b40f392ed8cfbd91de4236fd61414f752
ム乇 尺 1 乇 <121339626+ger1e@users.noreply.github.com>
2026-09-13 19:28:22 +0200

    test: add TLS fingerprint probe sample

--- src/control/provider-probe.js
+++ src/control/provider-probe.js
@@ -12,6 +12,7 @@ export const PROBE_SAMPLE_BY_TYPE = Object.freeze({
   asn: 'AS15169',
   cidr: '8.8.8.0/24',
   package: 'pkg:npm/lodash@4.17.21',
+  'tls-fingerprint': 'ja3:72a589da586844d7f0818ce684948eea',
 });
 
 const STATUS_PRIORITY = Object.freeze({
